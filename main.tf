b9ee2114b98b525c7b6117d5166121247b1e272c
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-02-03 03:42:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.6.1"
-    }
-  }
-}
-provider "aws" {
-  alias  = "us-east-1"
-  region = "us-east-1"
-}
-
 data "aws_route53_zone" "main" {
   name         = var.domain_name
   private_zone = false
