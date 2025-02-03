2a6700c3f5abd456605cdee6ef5fbe2313f629c9
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-02-02 19:15:22 -0500

    added s3+ cloudfront

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.8"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+data "aws_route53_zone" "main" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+module "website" {
+  source             = "SPHTech-Platform/s3-cloudfront-static-site/aws"
+  version            = "0.3.3"
+  additional_aliases = [var.domain_name, "www.${var.domain_name}"]
+  providers          = { aws.us-east-1 = aws }
+  bucket_name        = var.domain_name
+  price_class        = "PriceClass_100"
+  domains = {
+    default_domain = {
+      dns_zone_id         = data.aws_route53_zone.main.zone_id
+      domain              = var.domain_name
+      include_in_acm      = true
+      create_acm_record   = true
+      create_alias_record = true
+    }
+  }
+}
+
